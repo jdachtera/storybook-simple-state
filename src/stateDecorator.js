8c325f1c3eefe17b024b579f38a106275171ec25
Alexander Zhukov <al.zhukoff@gmail.com>
2019-02-27 08:03:43 +0100

    export State and withState (#2)
    * Update index.js
    
    * Update stateDecorator.js
    
    * Update stateDecorator.js

--- src/stateDecorator.js
+++ src/stateDecorator.js
@@ -13,7 +13,8 @@ export const State = ({ render, initial, storeKey = 'default' }) => {
   });
 }
 
-export const withState = (...args) => (decoratedStory) => {
+export const withState = (...originalArgs) => (decoratedStory) => {
+  const args = originalArgs.slice(); //copy original arguments due to mutation below
   const storyFn = typeof args[args.length - 1] === 'function' ? args.pop() : decoratedStory;
   const [initial, storeKey] = args;
   return <State storeKey={storeKey} initial={initial} render={storyFn} />
